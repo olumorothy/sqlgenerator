f42bc5d934d3c719e356ef3fcfdc15347633deb9
Olumorothy <iseoluwa89@gmail.com>
2023-04-19 19:50:46 +0100

    implements the submit button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import styles from "./index.module.css";
 import sqlLogo from "./assets/sql-server.png";
+import { useState } from "react";
 function App() {
+  const [queryDesc, setQueryDesc] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted :", queryDesc);
+  };
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="sql-logo" className={styles.icon} />
       <h3>Generate SQL with AI</h3>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="query-description"
           placeholder="Describe your query"
+          onChange={(e) => setQueryDesc(e.target.value)}
         />
         <input type="submit" value="Generate query" />
       </form>
